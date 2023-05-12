b06bc4c8a97b0bd12c0e672911e465761bf33c7e
kuruminminzemi <rosakanina12345@gmail.com>
2023-05-12 16:43:19 +0900

    こんにちは増殖

--- main.js
+++ main.js
@@ -6,35 +6,41 @@ const keyInput = new keyInputManager();
 GameArea.refresh();
 
 //回り続ける小山高専
-let oyamaLogo = new CanvasComponents({
-  ctx: MainContext,
-  img: "./assets/Oyama_logo.png",
-  position: new Vector2(GameArea.x / 2, GameArea.y / 4),
-});
-oyamaLogo.update = function () {
-  if(keyInput.IsPressed("ArrowLeft")){
-    this.position.x -= 10;
-  }
-  if(keyInput.IsPressed("ArrowRight")){
-    this.position.x += 10;
-  }
-  if(keyInput.IsPressed("ArrowUp")){
-    this.position.y -= 10;
-  }
-  if(keyInput.IsPressed("ArrowDown")){
-    this.position.y += 10;
-  }
-};
-
-function update() {
-  // your code goes here
+let components = [];
+for (let i = 0; i < 9; i++) {
+  components[i] = new CanvasComponents({
+    ctx: MainContext,
+    img: "./assets/Oyama_logo.png",
+    position: new Vector2(GameArea.x / 2 + 100 * i, GameArea.y / 4),
+  });
+  components[i].update = function () {
+    this.rotate += 10;
+    if (keyInput.IsPressed("ArrowLeft")){
+      this.position.x -= 10;
+    }
+    if (keyInput.IsPressed("ArrowRight")){
+      this.position.x += 10;
+    }
+    if (keyInput.IsPressed("ArrowUp")){
+      this.position.y -= 10;
+    }
+    if (keyInput.IsPressed("ArrowDown")){
+      this.position.y += 10;
+    }
+  };
 }
 
-//ゲームループの定義・開始
+function update() {
+//ゲームループの定義・開始 
+}
 const GameLoop = new GameLoopManager(() => {
   update();
   MainContext.clearRect(0, 0, GameArea.x, GameArea.y);
-  oyamaLogo.render();
-  oyamaLogo.update();
+
+
+  for (let i = 0; i < components.length; i++){
+  components[i].render();
+  components[i].update();
+  }
 }, 30);
 GameLoop.start();
